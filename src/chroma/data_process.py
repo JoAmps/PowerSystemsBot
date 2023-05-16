ff57fb1136c8fbc06cb137e6f627470cbfc066e6
Hyacinth Ampadu <ampaduh@gmail.com>
2023-05-16 15:51:22 +0000

    adding readme

--- src/chroma/data_process.py
+++ src/chroma/data_process.py
@@ -4,7 +4,7 @@ sys.path.append("data")
 
 # Create and load PDF Loader
 def create_load_pdf_loader():
-    loader = PyPDFLoader('EE 559 Protection of Power Systems.pdf')
+    loader = PyPDFLoader('src/chroma/EE 559 Protection of Power Systems.pdf')
     # Split pages from pdf 
     pages = loader.load_and_split()
     return pages
